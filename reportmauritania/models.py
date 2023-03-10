3b8f784653b3f4ad0f8927e5c6b9a22abc2c9af3
Tiah Kevel <k.choumene@cloudfacile.com>
2023-03-10 17:52:10 +0100

    Impression de la liste des bénéficiares ref#25146

--- reportmauritania/models.py
+++ reportmauritania/models.py
@@ -16,61 +16,132 @@ import base64
 from io import BytesIO
 import os
 
-def beneficiary_card_query(user, **kwargs):
+# def beneficiary_card_query(user, **kwargs):
 
+#     # Create qr code instance
+#     qr = qrcode.QRCode()
+
+#     insureeObj = Insuree.objects.filter(
+#             chf_id="070707070",
+#             validity_to__isnull=True
+#             ).first()
+
+#     # The data that you want to store
+#     data = {
+#         'chf_id': insureeObj.chf_id,
+#         'last_name': insureeObj.last_name,      
+#         'other_names': insureeObj.other_names
+#     }
+
+#     # Add data
+#     qr.add_data(data)
+
+#     # Generate the QR Code
+#     qr.make()
+
+#     # Create an image from the QR Code instance
+#     img = qr.make_image()
+
+#     # Create a BytesIO object
+#     buffered = BytesIO()
+
+#     # Save the image to the BytesIO object
+#     img.save(buffered, format="png")
+
+#     # Get the byte data
+#     img_str = base64.b64encode(buffered.getvalue())
+#     #img_encoded = base64.b64encode(img.getvalue())
+
+#     filename = "openIMISphone/"+insureeObj.photo.folder+"/"+insureeObj.photo.filename
+#     print(filename)
+#     if os.path.exists(filename):
+#         with open(filename, "rb") as image_file:
+#             encoded_img = base64.b64encode(image_file.read()).decode('utf-8')
+#     else :
+#         with open("default-img.png", "rb") as image_file:
+#             encoded_img = base64.b64encode(image_file.read()).decode('utf-8')
+#         print("File not found")
+
+
+#     dictBase =  {
+#         "QrCode": "data:image/PNG;base64,"+img_str.decode("utf-8"),
+#         "PhotoInsuree": "data:image/PNG;base64,"+encoded_img,
+#         "Prenom" : insureeObj.other_names,
+#         "Nom" : insureeObj.last_name,
+#         "DateNaissance" : insureeObj.dob,
+#         "DateExpiration" : insureeObj.dob,
+#         "idInsuree" : insureeObj.chf_id
+#         }
+
+#     print(dictBase)
+#     return dictBase
+
+def beneficiaries_list_card_query(user, **kwargs):
+    print("kwargs ", kwargs)
+    ids = kwargs.get("insureeids", [])
+    insurees_ids = []
+    if ids:
+        ids = ids.split(',')
+        insurees_ids = [eval(i) for i in ids]
     # Create qr code instance
     qr = qrcode.QRCode()
-
-    insureeObj = Insuree.objects.filter(
-            chf_id="070707070",
+    
+    insuree_list = Insuree.objects.filter(
+            id__in=insurees_ids,
             validity_to__isnull=True
-            ).first()
+            )
 
-    # The data that you want to store
-    data = {
-        'chf_id': insureeObj.chf_id,
-        'last_name': insureeObj.last_name,      
-        'other_names': insureeObj.other_names
-    }
+    insurees_data = []
+    for insureeObj in insuree_list:
+        # The data that you want to store
+        data = {
+            'chf_id': insureeObj.chf_id,
+            'last_name': insureeObj.last_name,      
+            'other_names': insureeObj.other_names
+        }
 
-    # Add data
-    qr.add_data(data)
+        # Add data
+        qr.add_data(data)
 
-    # Generate the QR Code
-    qr.make()
+        # Generate the QR Code
+        qr.make()
 
-    # Create an image from the QR Code instance
-    img = qr.make_image()
+        # Create an image from the QR Code instance
+        img = qr.make_image()
 
-    # Create a BytesIO object
-    buffered = BytesIO()
+        # Create a BytesIO object
+        buffered = BytesIO()
 
-    # Save the image to the BytesIO object
-    img.save(buffered, format="png")
+        # Save the image to the BytesIO object
+        img.save(buffered, format="png")
 
-    # Get the byte data
-    img_str = base64.b64encode(buffered.getvalue())
-    #img_encoded = base64.b64encode(img.getvalue())
+        # Get the byte data
+        img_str = base64.b64encode(buffered.getvalue())
+        #img_encoded = base64.b64encode(img.getvalue())
 
-    filename = "openIMISphone/"+insureeObj.photo.folder+"/"+insureeObj.photo.filename
-    print(filename)
-    if os.path.exists(filename):
-        with open(filename, "rb") as image_file:
-            encoded_img = base64.b64encode(image_file.read()).decode('utf-8')
-    else :
-        with open("default-img.png", "rb") as image_file:
-            encoded_img = base64.b64encode(image_file.read()).decode('utf-8')
-        print("File not found")
+        filename = "openIMISphone/"+insureeObj.photo.folder+"/"+insureeObj.photo.filename
+        print(filename)
+        if os.path.exists(filename):
+            with open(filename, "rb") as image_file:
+                encoded_img = base64.b64encode(image_file.read()).decode('utf-8')
+        else :
+            with open("default-img.png", "rb") as image_file:
+                encoded_img = base64.b64encode(image_file.read()).decode('utf-8')
+            print("File not found")
 
-
+        insurees_data.append({
+            "QrCode": "data:image/PNG;base64,"+img_str.decode("utf-8"),
+            "PhotoInsuree": "data:image/PNG;base64,"+encoded_img,
+            "Prenom" : insureeObj.other_names,
+            "Nom" : insureeObj.last_name,
+            "DateNaissance" : insureeObj.dob,
+            "DateExpiration" : insureeObj.dob,
+            "idInsuree" : insureeObj.chf_id
+            }
+        )
+        
     dictBase =  {
-        "QrCode": "data:image/PNG;base64,"+img_str.decode("utf-8"),
-        "PhotoInsuree": "data:image/PNG;base64,"+encoded_img,
-        "Prenom" : insureeObj.other_names,
-        "Nom" : insureeObj.last_name,
-        "DateNaissance" : insureeObj.dob,
-        "DateExpiration" : insureeObj.dob,
-        "idInsuree" : insureeObj.chf_id
+        "InsureeList" : insurees_data
         }
 
     print(dictBase)
--- reportmauritania/report.py
+++ reportmauritania/report.py
@@ -1,16 +1,18 @@
 from distutils.command import upload
-from reportmauritania.models import  beneficiary_card_query
-from reportmauritania.report_templates import rptBeneficiaryCard
+# from reportmauritania.models import  beneficiary_card_query
+# from reportmauritania.report_templates import rptBeneficiaryCard
+from reportmauritania.models import  beneficiaries_list_card_query
+from reportmauritania.report_templates import rptBeneficiaries_list_Card
 
 
 report_definitions = [ 
     {
         "name": "beneficiary_card_mauritania",
         "engine": 0,
-        "default_report":rptBeneficiaryCard.template,
+        "default_report":rptBeneficiaries_list_Card.template,
         "description": "Insuree Card",
         "module": "reportmauritania",
-        "python_query": beneficiary_card_query, 
+        "python_query": beneficiaries_list_card_query, 
         "permission": ["131215"],
     }
     
