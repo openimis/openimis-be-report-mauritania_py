1c3a2f61e9b172976b3439359b6c27ee3f21ecc8
kevel <keveltiah@gmail.com>
2023-10-11 17:20:29 +0100

    Rapport de facturation cnass

--- reportmauritania/report.py
+++ reportmauritania/report.py
@@ -1,8 +1,8 @@
 from distutils.command import upload
 # from reportmauritania.models import  beneficiary_card_query
 # from reportmauritania.report_templates import rptBeneficiaryCard
-from reportmauritania.models import  beneficiaries_list_card_query
-from reportmauritania.report_templates import rptBeneficiaries_list_Card
+from reportmauritania.models import  beneficiaries_list_card_query, invoice_report_query
+from reportmauritania.report_templates import rptBeneficiaries_list_Card, invoice_report_template
 
 
 report_definitions = [ 
@@ -13,6 +13,15 @@ report_definitions = [
         "description": "Insuree Card",
         "module": "reportmauritania",
         "python_query": beneficiaries_list_card_query, 
+        "permission": ["131215"],
+    },
+    {
+        "name": "invoice_mauritania",
+        "engine": 0,
+        "default_report":invoice_report_template.template,
+        "description": "Mauritania Invoice report",
+        "module": "reportmauritania",
+        "python_query": invoice_report_query, 
         "permission": ["131215"],
     }
     
